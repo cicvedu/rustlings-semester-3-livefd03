a7aef7f40909fe1d8ab72406af46ed68aca028fb
livefd03 <2635454053@qq.com>
2024-03-22 01:35:23 +0800

    ok?

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,9 +9,9 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        panic!("hello");
-    }
+    // if my_option.is_none() {
+    //     panic!("hello");
+    // }
 
     let my_arr = &[
         -1, -2, -3,
